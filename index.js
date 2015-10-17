a6d1ca5b4cd5658198d65498adeb066a71c9236c
zephraph <zephraph@gmail.com>
2015-10-17 00:32:11 -0400

    Hello world

---
+++ index.js
@@ -0,0 +1,42 @@
+var extend = require('extend');
+
+var Namespace = {};
+
+function namezpace(scope, definition) {
+  var item, root;
+  if (!Namespace.hasOwnProperty(__dirname)) {
+    Namespace[__dirname] = {};
+  }
+  root = Namespace[__dirname];
+
+  // Traverse scope and build an object tree as needed
+  // TODO: Add better error handling
+  if( typeof scope === 'string' ) {
+    scope = scope.split('.');
+    for (var i = 0; i < scope.length; ++i) {
+      item = scope[i];
+      if (root[item] == null) {
+        root[item] = {};
+      }
+      root = root[item];
+    }
+  } else {
+    return root;
+  }
+
+  if (typeof definition === 'function') {
+    definition.apply(root);
+  } else if (typeof definition === 'object' && definition !== null) {
+    root = extend(true, root, definition);
+  }
+
+  return root;
+};
+
+Object.defineProperty(namezpace, 'local', {
+  get: function() {
+    return Namespace[__dirname];
+  }
+});
+
+module.exports = namezpace;
